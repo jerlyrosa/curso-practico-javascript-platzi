15e4c620ede49f54cd72dbaba93686ba4b930c11
sr-jerly <jerlymigueldelarosah@gmail.com>
2021-09-26 14:27:43 -0400

    Analisis salarial: Calcular la  media del top 10% de los salarios -> Completado

--- js/analisis.js
+++ js/analisis.js
@@ -1,25 +1,38 @@
 const salaryCol = colombia.map((item)=>{
    return  item.salary;
-});
+}).sort((a,b)=> a -b);
+
+//FUNCION MEDIANA SALARIAL
 
 const getAverage = (values) =>{
     const sumResult  = values.reduce((acc = 0, cur)=> acc + cur);
     return sumResult / values.length;
-
 }  
 
 const  medianSalary =(values)=>{
-    const sortList = values.sort((a,b)=> a -b);
+    const sortList = values;
     const isEvent =  values.length % 2  === 0;
     const middleValue = Math.floor(sortList.length / 2);
     const middleFirts = middleValue;
     const middleSecond =  middleFirts - 1;
     const valueResults = [sortList[middleValue], sortList[middleSecond]];
 
-return  isEvent ? ( getAverage( valueResults )):( sortList[middleValue] )
+return  isEvent ? ( getAverage( valueResults )):( sortList[middleValue] );
 
 }
 
+const medianaGeneralSalario = medianSalary(salaryCol);
+
+const spliceStart = (salaryCol.length * 80) / 100;
+const spliceCount = salaryCol.length - spliceStart;
+
+
+const salaryTop10 = salaryCol.splice( spliceStart, spliceCount);
+  
+const medianaTop10Col = medianSalary(salaryTop10)
+
+
 console.log(
-    medianSalary(salaryCol)
+    medianaGeneralSalario,   
+    medianaTop10Col
 )
